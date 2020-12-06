c96b043580e2b0572fa09dca49398c15a84cc335
kav2019 <kav2019@mail.ru>
2020-12-06 12:34:30 +0300

    add home work 3

--- Lesson3/src/main/java/Main.java
+++ Lesson3/src/main/java/Main.java
@@ -5,26 +5,58 @@ public class Main {
         String[] array = {"home", "word", "auto", "aple", "home", "car", "friend", "room", "aple", "stack", "book",
                 "foot", "bike", "hello", "hello"
         };
+        arrayUniq(array);
+
+        PhonDict dict = new PhonDict();
+        dict.add("Ivanov", "8-999-199-25-99");
+        dict.add("Ivanov", "8-999-199-25-98");
+        dict.add("Lopuchov", "8-999-199-00-00");
+        dict.add("Frolov", "8-999-199-01-00");
+        dict.add("Frolov", "8-999-199-02-00");
+        System.out.println(dict.get("Ivanov"));
+        System.out.println(dict.get("Lopuchov"));
+        System.out.println(dict.get("Frolov"));
+
+
+    }
+
+    public static void arrayUniq(String[] array){
         Set<String> arrayUniq = new HashSet<>(); // Создаем HashSet для добавления уникальных значений
         for (String i: array) { // Добавляем массива слов в HashSet
             arrayUniq.add(i);
         }
         System.out.println(arrayUniq); // Выводим уникальные значение
 
-        for (String key: arrayUniq) {
-            System.out.println(key + ": " + Collections.frequency(Arrays.asList(array), key));
+        Map<String, Integer> countWords = new HashMap<>(); // Подсчитываем количество слов
+        for (String words : arrayUniq){
+            int count = 0;
+            for (int i = 0; i < array.length; i++){
+                if(words.equals(array[i])){
+                    count++;
+                }
+            }
+            countWords.put(words, count);
+        }
+        System.out.println(countWords);
+
+    }
+
+     static class PhonDict{
+        final Map<String, List<String>> book = new HashMap<>();
+
+        public void add(String family, String number){
+            if (book.containsKey(family)){
+                book.get(family).add(number);
+            }else {
+                List<String> newNumber = new ArrayList<>();
+                newNumber.add(number);
+                book.put(family, newNumber);
+            }
         }
 
-//        Iterator<String> iterator = arrayUniq.iterator();
-//        while (iterator.hasNext()){
-//            String time = iterator.;
-//            for (int i = 0; i < array.length; i++){
-//                int count = 0;
-//                if (time.equals(array[i])){
-//                    count++;
-//                }
-//                System.out.println("Слово " + iterator.toString() + " встречается " + count + " раз");
-//            }
-//        }
+        public List<String> get(String family){
+            return book.get(family);
+        }
+
     }
 }
