0fdb8cda7b87d8eab1e4ed86afa6782a3f94443d
kav2019 <kav2019@mail.ru>
2020-12-17 20:04:46 +0300

    dowland home work 6

--- Lesson6/src/main/java/Main.java
+++ Lesson6/src/main/java/Main.java
@@ -7,45 +7,60 @@ import java.util.Scanner;
 
 class EchoServer{
     public static void main(String[] args) {
-        Socket socket = null;
         try {
-            ServerSocket serverSocket = new ServerSocket(8000);
-            System.out.println("Сервер запущен, ожидаем подключение...");
-            socket = serverSocket.accept();
-            System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            while (true){
-                String str = in.readUTF();
-                if(str.equals("end")){
-                    break;
-                }
-                out.writeUTF("Эхо: " + str);
-            }
-
+            ServerSocket serverSocket = new ServerSocket(8081);
+            Socket socket = serverSocket.accept();
+            new Worker(socket, "Клиент");
         }catch (IOException e){
-
+            e.printStackTrace();
         }
-
     }
 }
 
 class EchoClient {
     public static void main(String[] args) {
-        // Подключаемся к серверу
-        try (Socket socket = new Socket("localhost", 8000)) {
-            //Создаем обертки над потоками данных
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            //Читаем консоль и отправляем строки на сервер
-            String line;
-            Scanner scanner = new Scanner(System.in);
-            do{
-                line = scanner.nextLine();
-                dataOutputStream.writeUTF(line);
-                System.out.println(dataInputStream.readUTF());
-            } while (!line.equals("stop"));
+        try {
+            Socket socket = new Socket("localhost", 8081);
+            new Worker(socket, "Сервер");
         }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Worker{
+
+    public Worker(Socket socket, String name){
+        try {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Thread threadMess = new Thread(() -> {
+                while (true){
+                    try {
+                        String str = in.readUTF();
+                        if(str.equals("/stop")){
+                            break;
+                        }
+                        System.out.println(name + ": " + str);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Thread threadAnswer = new Thread(() -> {
+                while (true){
+                    try {
+                        String answer = scanner.nextLine();
+                        out.writeUTF( answer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threadAnswer.start();
+            threadMess.start();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
